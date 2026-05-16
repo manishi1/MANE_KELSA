a6bef13629eeae87d21d611d63ea7031b4974bc0
manishi1 <shilpasgowda194@gmail.com>
2026-05-16 23:14:44 +0530

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.google.material)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.androidx.cardview)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.firestore)
+}
